f720a7ee3999ede2466578ac411123dcd66059a7
jurajanusev <126396154+jurajanusev@users.noreply.github.com>
2026-06-30 09:10:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -309,4 +309,4 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
     
-    rocessed_actions.add(action_id)
+    processed_actions.add(action_id)
